2c7684d421834164d678ed198ee1ad7326f32676
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2023-10-19 20:30:25 +0200

    Revert ".gitignore fixed"
    This reverts commit 83066adc444a9e03b6c5010b5ceed0d5469214c7.

--- cmake-build-debug/CMakeFiles/gameOfLife.dir/GameGUI.cpp.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/gameOfLife.dir/GameGUI.cpp.obj: \
- C:\Users\PC\CLionProjects\Game-of-life-CLI\GameGUI.cpp \
- C:\Users\PC\CLionProjects\Game-of-life-CLI\GameGUI.h \
- C:\Users\PC\CLionProjects\Game-of-life-CLI\Game.h \
- C:\Users\PC\CLionProjects\Game-of-life-CLI\Engine.h \
- C:\Users\PC\CLionProjects\Game-of-life-CLI\Array.h
