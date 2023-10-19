83066adc444a9e03b6c5010b5ceed0d5469214c7
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2023-10-19 20:21:30 +0200

    .gitignore fixed

---
+++ cmake-build-debug/CMakeFiles/gameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameOfLife.dir/Array.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/Array.cpp.obj.d"
+  "CMakeFiles/gameOfLife.dir/Engine.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/Engine.cpp.obj.d"
+  "CMakeFiles/gameOfLife.dir/Game.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/Game.cpp.obj.d"
+  "CMakeFiles/gameOfLife.dir/GameCLI.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/GameCLI.cpp.obj.d"
+  "CMakeFiles/gameOfLife.dir/GameGUI.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/GameGUI.cpp.obj.d"
+  "CMakeFiles/gameOfLife.dir/main.cpp.obj"
+  "CMakeFiles/gameOfLife.dir/main.cpp.obj.d"
+  "gameOfLife.exe"
+  "gameOfLife.exe.manifest"
+  "gameOfLife.pdb"
+  "libgameOfLife.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
